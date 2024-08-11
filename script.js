835d3f0336d7d2d4c6bcc54e2b88a62fafb1425e
Aldo Iván <70114981+Aldoihm@users.noreply.github.com>
2024-08-11 13:40:04 -0600

    Empezando mi proyecto de Marvel

---
+++ script.js
@@ -0,0 +1,7 @@
+const myForm = document.getElementById("myForm")
+const startsWith = document.getElementById("startWith")
+const btnSearch = document.getElementById("btnSearch")
+const myContent = document.getElementById("myContent")
+const apiKey = "eecf7d5e4faebc8beb80c430af40f474"
+
+myForm.addEventListener("submit", e => e.preventDefault())
